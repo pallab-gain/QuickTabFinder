ed6cf89c1e041fe0a258d7b2cf1f145a82fe2b80
pallab-gain <pallab.gain@gmail.com>
2024-02-02 15:07:11 +0200

    add react app

--- src/popup/popup.ts
+++
@@ -1,9 +0,0 @@
-const onAllTabs = (tabs: any) => {
-    console.info('-> open tabs', tabs)
-}
-const listTabs = (): void => {
-    // @ts-ignore
-    chrome.tabs.query({}, onAllTabs)
-}
-
-document.addEventListener('DOMContentLoaded', listTabs)
